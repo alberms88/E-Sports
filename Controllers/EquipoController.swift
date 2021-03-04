ca95e7ddcc63f28b59b5de1a6a01dab88bb17c0b
alberto <alberto_martinez_apps1ma1920@cev.com>
2021-03-04 14:46:46 +0100

    Avance en pantalla noticias y en pantalla equipo, ademas corrección de errores varios.

---
+++ Controllers/EquipoController.swift
@@ -0,0 +1,20 @@
+//
+//  EquipoController.swift
+//  E-sports
+//
+//  Created by Apps2m on 04/03/2021.
+//
+
+import UIKit
+
+class EquipoController: UIViewController {
+
+    @IBOutlet weak var NoticiasView
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+   
+    }
+    
+
+}
